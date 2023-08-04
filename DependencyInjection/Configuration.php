e50b67139accb09f7cc717374ffb661eb40aebd3
lcp0578 <lcp0578@gmail.com>
2023-08-04 18:14:09 +0800

    upgrade for symfony 5.4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('kit_paginator');
+        $treeBuilder = new TreeBuilder('kit_paginator');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
