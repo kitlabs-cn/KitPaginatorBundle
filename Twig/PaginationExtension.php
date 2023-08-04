99b691f8df4d809b4ad848dd372849e4fd835154
lcp0578 <lcp0578@163.com>
2023-08-05 00:31:51 +0800

    upgrade new twig

--- Twig/PaginationExtension.php
+++ Twig/PaginationExtension.php
@@ -1,32 +1,66 @@
 <?php
 namespace Kit\PaginatorBundle\Twig;
 
-class PaginationExtension extends \Twig_Extension
+class PaginationExtension implements \Twig\Extension\ExtensionInterface
 {
-    /**
-     * {@inheritDoc}
-     */
-    public function getFunctions()
-    {
-        return array(
-            new \Twig_SimpleFunction('kit_pagination_render', array($this, 'render'), array('is_safe' => array('html'), 'needs_environment' => true)),
-        );
-    }
-    
-    /**
-     * Renders the pagination template
-     *
-     * @param \Twig_Environment $env
-     * @param SlidingPagination $pagination
-     * @param string            $template
-     * @param array             $queryParams
-     * @param array             $viewParams
-     *
-     * @return string
-     */
-    public function render(\Twig_Environment $env, $pagination, $template = null, array $queryParams = array(), array $viewParams = array())
-    {
-        $pagination['query'] = array_merge($pagination['query'], $queryParams);
-        return $env->render( $template, array_merge($pagination, $viewParams));
-    }
+	/**
+	 * Returns the token parser instances to add to the existing list.
+	 *
+	 * @return \Twig\TokenParser\TokenParserInterface[]
+	 */
+	public function getTokenParsers(){}
+	
+	/**
+	 * Returns the node visitor instances to add to the existing list.
+	 *
+	 * @return \Twig\NodeVisitor\NodeVisitorInterface[]
+	 */
+	public function getNodeVisitors(){}
+	
+	/**
+	 * Returns a list of filters to add to the existing list.
+	 *
+	 * @return \Twig\TwigFilter[]
+	 */
+	public function getFilters(){}
+	
+	/**
+	 * Returns a list of tests to add to the existing list.
+	 *
+	 * @return \Twig\TwigTest[]
+	 */
+	public function getTests(){}
+	
+	/**
+	 * Returns a list of operators to add to the existing list.
+	 *
+	 * @return array<array> First array of unary operators, second array of binary operators
+	 */
+	public function getOperators(){}
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getFunctions()
+	{
+		return array(
+				new \TwigFunction('kit_pagination_render', array($this, 'render'), array('is_safe' => array('html'), 'needs_environment' => true)),
+		);
+	}
+	
+	/**
+	 * Renders the pagination template
+	 *
+	 * @param \Twig_Environment $env
+	 * @param SlidingPagination $pagination
+	 * @param string            $template
+	 * @param array             $queryParams
+	 * @param array             $viewParams
+	 *
+	 * @return string
+	 */
+	public function render(\Twig_Environment $env, $pagination, $template = null, array $queryParams = array(), array $viewParams = array())
+	{
+		$pagination['query'] = array_merge($pagination['query'], $queryParams);
+		return $env->render( $template, array_merge($pagination, $viewParams));
+	}
 }
